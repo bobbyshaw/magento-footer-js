a517c17cdff0652c5a761526c2514067648fde06
Tom Robertshaw <me@tomrobertshaw.net>
2015-06-04 18:11:44 +0100

    Remove layout block and instead put javascript at the end of every block. Except when we see the closing body and html tags; then move those to the very end.
    - This does put Google Analytics at the end of the page which means some visitors may be missed if they leave the page before the footer js is interpreted.

--- app/code/community/Meanbee/Footerjs/Model/Observer.php
+++ app/code/community/Meanbee/Footerjs/Model/Observer.php
@@ -1,48 +1,35 @@
 <?php
 class Meanbee_Footerjs_Model_Observer {
 
+    const REGEX_JS              = '#<script.*</script>#isU';
+    const REGEX_CONDITIONAL_JS  = '#<\!--\[if[^\>]*>\s*<script.*</script>\s*<\!\[endif\]-->#isU';
+    const REGEX_DOCUMENT_END    = '#</body>.*</html>#isU';
+
+    /**
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
     public function handleInlineJs(Varien_Event_Observer $observer) {
-        /** @var Mage_Core_Block_Abstract $block */
-        $block = $observer->getBlock();
         /** @var Varien_Object $transport */
         $transport = $observer->getTransport();
-        $html = $transport->getHtml();
 
-        $ignoredBlocks = array('inline.js');
-        if (in_array($block->getNameInLayout(), $ignoredBlocks)) {
-            return false;
-        }
+        $patterns = array(
+            'js'             => self::REGEX_JS,
+            'conditional_js' => self::REGEX_CONDITIONAL_JS,
+            'document_end'   => self::REGEX_DOCUMENT_END
+        );
 
-        /** @var Mage_Core_Block_Text $inlineBlock */
-        $inlineBlock = $block->getLayout()->getBlock('inline.js');
+        foreach($patterns as $pattern) {
+            $matches = array();
 
-        $jsPattern = '#<script.*</script>#isU';
-        $conditionalJsPattern = '#<\!--\[if[^\>]*>\s*<script.*</script>\s*<\!\[endif\]-->#isU';
-
-        // First deal with conditionals
-        $matches = array();
-        $success = preg_match_all($conditionalJsPattern, $html, $matches);
-        if ($success) {
-            $text = implode('', $matches[0]);
-
-            $before = ($block->getNameInLayout() == "head") ? true : false;
-            $inlineBlock->addText($text, $before);
-
-            $html = preg_replace($conditionalJsPattern, '' , $html);
-            $transport->setHtml($html);
-        }
-
-        // Then the rest of the javascript
-        $matches = array();
-        $success = preg_match_all($jsPattern, $html, $matches);
-        if ($success) {
-            $text = implode('', $matches[0]);
-
-            $before = ($block->getNameInLayout() == "head") ? true : false;
-            $inlineBlock->addText($text, $before);
-
-            $html = preg_replace($jsPattern, '' , $html);
-            $transport->setHtml($html);
+            $html = $transport->getHtml();
+            $success = preg_match_all($pattern, $html, $matches);
+            if ($success) {
+                $text = implode('', $matches[0]);
+                $html = preg_replace($pattern, '', $html);
+                $transport->setHtml($html . $text);
+            }
         }
 
         return $this;
