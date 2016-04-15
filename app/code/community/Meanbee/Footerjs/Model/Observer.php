95a6639f8cf0236abc7498465e4c73b179ab3bae
Tom Robertshaw <me@tomrobertshaw.net>
2016-04-15 10:34:10 +0100

    Remove http response observer and rely solely on the block observer.

--- app/code/community/Meanbee/Footerjs/Model/Observer.php
+++ app/code/community/Meanbee/Footerjs/Model/Observer.php
@@ -1,31 +1,7 @@
 <?php
 
-class Meanbee_Footerjs_Model_Observer {
-
-    /**
-     * @param Varien_Event_Observer $observer
-     *
-     * @return $this
-     */
-    public function handleHttpResponseInlineJs(Varien_Event_Observer $observer)
-    {
-        Varien_Profiler::start('MeanbeeFooterJs');
-
-        /** @var Meanbee_Footerjs_Helper_Data $helper */
-        $helper = Mage::helper('meanbee_footerjs');
-        if (!$helper->isEnabled()) {
-            Varien_Profiler::stop('MeanbeeFooterJs');
-            return $this;
-        }
-
-        /** @var Mage_Core_Controller_Response_Http $response */
-        $response = $observer->getResponse();
-        $response->setBody($helper->moveJsToEnd($response->getBody()));
-
-        Varien_Profiler::stop('MeanbeeFooterJs');
-
-        return $this;
-    }
+class Meanbee_Footerjs_Model_Observer
+{
 
     /**
      * @param Varien_Event_Observer $observer
