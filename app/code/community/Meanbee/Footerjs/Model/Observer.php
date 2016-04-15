d171754f06a8171d8a75fe7abeaea159f6c81a10
Tom Robertshaw <me@tomrobertshaw.net>
2016-04-15 10:32:54 +0100

    Revert "Merge pull request #14 from mkutyba/master"
    This reverts commit 0ba0d5a5683fcf44cae8fec626267c1728117fd3, reversing
    changes made to d4f8bc85d6e478d32f2dd282d2c4e93b15d32a07.

--- app/code/community/Meanbee/Footerjs/Model/Observer.php
+++ app/code/community/Meanbee/Footerjs/Model/Observer.php
@@ -27,4 +27,50 @@ class Meanbee_Footerjs_Model_Observer {
         return $this;
     }
 
+    /**
+     * @param Varien_Event_Observer $observer
+     *
+     * @return $this
+     */
+    public function handleBlockInlineJs(Varien_Event_Observer $observer)
+    {
+        Varien_Profiler::start('MeanbeeFooterJs');
+
+        /** @var Meanbee_Footerjs_Helper_Data $helper */
+        $helper = Mage::helper('meanbee_footerjs');
+        if (!$helper->isEnabled()) {
+            Varien_Profiler::stop('MeanbeeFooterJs');
+            return $this;
+        }
+
+        /** @var Varien_Object $transport */
+        $transport = $observer->getTransport();
+
+        /** @var Mage_Core_Block_Abstract $block */
+        $block = $observer->getBlock();
+
+        if (in_array($block->getNameInLayout(), $helper->getBlocksToSkipMoveJs())) {
+            $transport->setHtml($helper->addJsToExclusion($transport->getHtml()));
+        }
+
+        if (Mage::app()->getRequest()->getModuleName() == 'pagecache') {
+            $transport->setHtml($helper->removeJs($transport->getHtml()));
+            Varien_Profiler::stop('MeanbeeFooterJs');
+            return $this;
+        }
+
+        if (!is_null($block->getParentBlock())) {
+            Varien_Profiler::stop('MeanbeeFooterJs');
+            // Only look for JS at the root block
+            return $this;
+        }
+
+        $transport->setHtml($helper->moveJsToEnd($transport->getHtml()));
+
+        Varien_Profiler::stop('MeanbeeFooterJs');
+
+        return $this;
+    }
+
+
 }
